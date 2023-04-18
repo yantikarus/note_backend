a2a7ec1e1a25d113c20caca84f5c4569f4480b74
Yanti <wsutr@hotmail.com>
2023-04-17 23:25:12 -0600

    add catch error handling and put and delete method

--- index.js
+++ index.js
@@ -44,15 +44,22 @@ app.get('/api/notes', (req, res) => {
         res.json(notes)
     })
 })
-app.get('/api/notes/:id', (req, res) => {
+app.get('/api/notes/:id', (req, res, next) => {
     Note.findById(req.params.id).then(note => {
-        res.json(note)
+        if(note){
+            res.json(note)
+        }else{
+            res.status(404).end()
+        }
     })
+    .catch(error => next(error))
 })
-app.delete('/api/notes/:id', (req, res) => {
-    const id = Number(req.params.id)
-    notes = notes.filter(note=> note.id !==id)
-    res.status(204).end()
+app.delete('/api/notes/:id', (req, res, next) => {
+    Note.findByIdAndRemove(req.params.id)
+    .then(result => {
+        res.status(204).end()
+    })
+    .catch(error =>next(error))
 })
 
 const generateId =()=> {
@@ -80,12 +87,33 @@ app.post('/api/notes', (req, res)=> {
    })
 })
 
+app.put('/api/notes/:id', (req, res, next)=>{
+    const body = req.body
+    const note = {
+        content:body.content,
+        important:body.important,
+    }
+    Note.findByIdAndUpdate(req.params.id, note, {new: true})
+    .then(updatedNote =>{
+        res.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
   }
   
 app.use(unknownEndpoint)
 
+const errorHandler = (error, req, res, next) =>{
+    console.log(error.message)
+    if(error.name === 'CastError'){
+        return res.status(400).send({error: 'malformatted id'})
+    }
+    next(error)
+}
+app.use(errorHandler)
 const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
